a2c721983d6e6242ddacdaee1a19498641386a98
hamoudilaz <hamoudi_lazkani@hotmail.se>
2025-06-05 14:50:57 +0200

    resolve ci

--- config/mockGlobals.js
+++ config/mockGlobals.js
@@ -2,9 +2,12 @@ import { jest } from '@jest/globals';
 
 export async function mockGlobals() {
     jest.unstable_mockModule('../config/constant.js', () => ({
-        connection: {},
+        Connection: class {
+            constructor() { }
+        },
         calculateFee: jest.fn(() => 0),
     }));
+
 
     jest.unstable_mockModule('../engine/execute.js', () => ({
         swap: jest.fn().mockResolvedValue({ result: 'MOCK_TX' })
